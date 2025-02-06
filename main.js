78dbeb48ec99c2c87adafaed97656da789e0ecc4
Abdalla Elhabak <abdallahelhabak@gmail.com>
2025-02-06 14:26:27 +0100

    I have added the attck and magic spells features

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const EMOJIS = {
   levelUp: "⬆️",
 };
 class Character {
+    // Constructor function to create a new character
     constructor(name) {
         this.name = name;
         this.hp = 100;
@@ -15,5 +16,22 @@ class Character {
         this.defense = Math.floor(Math.random() * 10) + 5;
         this.healcount = 3;
     }
-}
+    
+    // attackEnemy function to attack the enemy
+    attackEnemy(enemy) {
+        const damage = Math.max(this.attack - enemy.defense);
+        enemy.hp -= damage;
+        console.log(`%c${EMOJIS.attack} ${this.name} attacks ${enemy.name} for ${damage} damage!`, `color: red; font-weight: bold` );
+        console.log(`${enemy.name} has ${enemy.hp} HP left`);
+    }
+    // cast Magic function to cast magic on the enemy
+    castMagic(enemy) {
+        const damage = Math.max(this.attack - enemy.defense);
+        enemy.hp -= damage;
+        console.log(`%c${EMOJIS.magic} ${this.name} casts magic on ${enemy.name} for ${damage} damage!`, `color: blue; font-weight: bold`);
+        console.log(`${enemy.name} has ${enemy.hp} HP left`);
+    }
 
+
+
+}
