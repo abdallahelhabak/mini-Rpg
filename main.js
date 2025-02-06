e0e1c4033212dee2ba114612bf5dd3928b2b7ea1
Abdalla Elhabak <abdallahelhabak@gmail.com>
2025-02-06 14:15:06 +0100

    I have added the Main character

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const EMOJIS = {
-    attack: "⚡️",
-    magic: "🔮",
-    heal: "🩹",
-    ultimate: "💥",
-    win: "🏆",
-    defeat: "💀",
-    levelUp: "⬆️"
+  attack: "⚡️",
+  magic: "🔮",
+  heal: "🩹",
+  ultimate: "💥",
+  win: "🏆",
+  defeat: "💀",
+  levelUp: "⬆️",
 };
+class Character {
+    constructor(name) {
+        this.name = name;
+        this.hp = 100;
+        this.attack = Math.floor(Math.random() * 15) + 10;
+        this.defense = Math.floor(Math.random() * 10) + 5;
+        this.healcount = 3;
+    }
+}
+
